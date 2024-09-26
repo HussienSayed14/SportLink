a64477426db04972f8bc1f9ec2deb1a84f99a370
HussienSayed14 <hussiens399@gmail.com>
2024-09-26 14:52:53 +0300

    Initial Commit

---
+++ SportsLink/src/main/java/com/SportsLink/SportsLinkApplication.java
@@ -0,0 +1,13 @@
+package com.SportsLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsLinkApplication.class, args);
+	}
+
+}
