a588fc17cfb54c971a1bb4baa4c6d9db6a2f9dc8
HussienSayed14 <hussiens399@gmail.com>
2024-10-03 15:02:44 +0300

    Adding User Indexes

--- src/main/java/com/SportsLink/userAccount/authentication/UserModel.java
+++ src/main/java/com/SportsLink/userAccount/authentication/UserModel.java
@@ -2,6 +2,9 @@ package com.SportsLink.userAccount.authentication;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -13,24 +16,39 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 @Builder
-@Table(name = "user") //
-// ,indexes = {@Index(name = "idx_event_status",columnList = "status")})
+@Table(name = "users", indexes = {
+        @Index(name = "idx_phone_number",columnList = "phone_number",unique = true),
+        @Index(name= "idx_last_login",columnList = "last_login")
+})
 
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
+    @NotNull(message = "user phone cannot be null")
+    @Size(min = 8, max = 15)
+    @Column(name = "phone_number", length = 15, nullable = false)
     private String phone_number;
+    @Email
+    @Column(name = "email", nullable = true)
     private String email;
+    @NotNull
+    @Column(name = "name", nullable = false)
     private String name;
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private RolesEnum role;
+    @NotNull
+    @Column(name = "password_hash", nullable = false)
     private String password_hash;
-    private int failed_attempts;
-    private boolean is_blocked;
+    private int failed_attempts = 0;
+    private boolean is_blocked = false;
+    @Column(name = "blocked_until", nullable = true)
     private Timestamp blocked_until;
-    private boolean is_verified;
-    private boolean is_activated;
+    private boolean is_verified = false;
+    private boolean is_activated = true;
     private Timestamp created_at;
+    @Column(name = "last_login")
     private Timestamp last_login;
 
 }
