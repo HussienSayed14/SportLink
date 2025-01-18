4dc85cbd09e1a5d9c29f0a5c8d40b4dc7f603a88
HussienSayed14 <hussiens399@gmail.com>
2025-01-18 18:36:52 +0200

    Pitch Model Entity validation

--- src/main/java/com/SportsLink/pitch/PitchModel.java
+++ src/main/java/com/SportsLink/pitch/PitchModel.java
@@ -1,10 +1,11 @@
 package com.SportsLink.pitch;
 
-
 import com.SportsLink.booking.BookingModel;
 import com.SportsLink.fields.FieldModel;
 import com.SportsLink.hourlySlot.HourlySlotModel;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -15,10 +16,13 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
-@Table(name = "pitch", indexes = {
-
-//        @Index(name = "idx_city_hour_price", columnList = "city_id, hour_price")
-})
+@Table(
+        name = "pitch",
+        indexes = {
+                // Uncomment and modify as needed
+                // @Index(name = "idx_field_pitch_type", columnList = "field_id, pitch_type")
+        }
+)
 public class PitchModel {
 
     @Id
@@ -26,25 +30,32 @@ public class PitchModel {
     private int id;
 
     @Column(name = "pitch_number", nullable = false)
+    @NotNull(message = "Pitch number cannot be null")
+    @Min(value = 1, message = "Pitch number must be greater than or equal to 1")
     private int pitchNumber;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pitch_type", nullable = false)
+    @NotNull(message = "Pitch type cannot be null")
     private PitchType pitchType;
 
+    @Column(name = "number_of_players")
     private int numberOfPlayers;
 
+    @Column(name = "is_active", nullable = false)
     private boolean isActive;
+
+    @Column(name = "allow_booking", nullable = false)
     private boolean allowBooking;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "field_id")
+    @JoinColumn(name = "field_id", nullable = false)
+    @NotNull(message = "Field cannot be null")
     private FieldModel field;
-
 
     @OneToMany(mappedBy = "pitch", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<HourlySlotModel> hourlySlots;
 
     @OneToMany(mappedBy = "pitch", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BookingModel> booking;
-
-
 }
